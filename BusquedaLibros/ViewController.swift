43e8dd6ed1f939939fe18bfbbda5657fe58da3d1
Fernando Renteria <klunx316@hotmail.com>
2016-02-10 00:43:09 +1100

    Aplicación busqueda libros

---
+++ BusquedaLibros/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  BusquedaLibros
+//
+//  Created by Fernando Renteria on 9/02/2016.
+//  Copyright © 2016 Fernando Renteria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var isbn: UITextField!
+    @IBOutlet weak var errorlbl: UILabel!
+    @IBOutlet weak var titulolbl: UILabel!
+    @IBOutlet weak var autorlbl: UILabel!
+    
+    @IBOutlet weak var publisherlbl: UILabel!
+    var mainObject : String = "ISBN:"
+    
+    
+    @IBAction func buscarLibro(sender: UITextField) {
+        let valorisbn : String = isbn.text!
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + valorisbn
+        mainObject = mainObject + valorisbn
+        let url = NSURL(string: urls)
+        let JSONData : NSData? = NSData(contentsOfURL: url!)
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(JSONData!, options:NSJSONReadingOptions.MutableLeaves)
+            let diccionario = json[mainObject] as! NSDictionary
+            let titulo = diccionario["title"] as! NSString as String
+            let publishers = diccionario["publishers"] as! NSArray
+            let autores = diccionario["authors"] as! NSArray
+            self.titulolbl.text = titulo
+            for autor in autores {
+                let tmpAutor : String = autor["name"] as! NSString as String
+                self.autorlbl.text = self.autorlbl.text! + "\(tmpAutor)\n"
+            }
+            
+            for publisher in publishers {
+                let tmpPublisher : String = publisher["name"] as! NSString as String
+                self.publisherlbl.text = self.publisherlbl.text! + "\(tmpPublisher)\n"
+            }
+            
+        }
+            catch let JSONError as NSError {
+                errorlbl.text = "\(JSONError)"
+                
+            }
+
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
